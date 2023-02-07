320b86774b2610f94193b6d36db0f7c54cb05bc5
Shoaib Ahmed <ashoaib540918@gmail.com>
2023-02-07 13:43:31 +0530

    remove template literal

--- src/templates/test.tsx
+++ src/templates/test.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { COMPONENT_NAME } from './COMPONENT_NAME';
 
-describe(`${COMPONENT_NAME} Tests`, () => {
+describe(`COMPONENT_NAME Tests`, () => {
   beforeEach(() => {
     render(<COMPONENT_NAME>Component</COMPONENT_NAME>);
   });
