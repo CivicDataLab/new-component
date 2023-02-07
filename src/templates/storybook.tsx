3e0fc039eb3abdcfa643ab7217f917b2a8a044e7
Shoaib Ahmed <ashoaib540918@gmail.com>
2023-02-07 13:44:53 +0530

    template literal -> string

--- src/templates/storybook.tsx
+++ src/templates/storybook.tsx
@@ -3,7 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { COMPONENT_NAME } from './COMPONENT_NAME';
 
 export default {
-  title: `Components/COMPONENT_NAME`,
+  title: 'Components/COMPONENT_NAME',
   component: COMPONENT_NAME,
 
   argTypes: {
@@ -17,5 +17,5 @@ export default {
 const Template: ComponentStory<typeof COMPONENT_NAME> = (props) => <COMPONENT_NAME {...props} />;
 export const Primary = Template.bind({});
 Primary.args = {
-  children: `COMPONENT_NAME`,
+  children: 'COMPONENT_NAME',
 };
